be650754d17190d220c5d147f4b936c5a8686dcf
Bin Wang <wbin00@gmail.com>
2018-07-26 12:38:13 +0800

    init commit

---
+++ del_mail.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+# Setup the Gmail API
+SCOPES = 'https://mail.google.com/'
+store = file.Storage('token.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = build('gmail', 'v1', http=creds.authorize(Http()))
+
+print("Input the search keyword:")
+keyword = input()
+print("The keyword is " + keyword)
+
+# Get messages
+req = service.users().messages().list(userId='me', q=keyword)
+res = req.execute()
+messages = res.get('messages')
+
+# Show message titles
+print("Here is some of the mails.")
+print()
+for m in messages[:10]:
+    mail = service.users().messages().get(userId='me', id=m['id'],
+            format='metadata').execute()
+    print(mail['snippet'])
+print()
+print("Is these looks like the mails you want to delete? Print enter to continue, Ctrl+C to exit.")
+input()
+
+total = 0
+
+print("Start delete mails, search keyword: " + keyword)
+while messages != None and len(messages) > 0:
+    count = len(messages)
+    print("Deleting %d emails" % count)
+    ids = {'ids': [m['id'] for m in messages]}
+    deleted = service.users().messages().batchDelete(userId='me',
+            body=ids).execute()
+    total += count
+    print("Deleted %d emails" % total)
+    req = service.users().messages().list_next(req, res)
+    res = req.execute()
+    messages = res.get('messages')
---
+++ readme.md
@@ -0,0 +1,15 @@
+
+
+Delete all the Emails from Gmail that match a search keyword.
+
+## Why?
+
+You cannot delete tens of thousands mails at once on Gmail UI. It will fail at last. So I write a script to do.
+
+## Usage
+
+1. Install Google client library: `pip install --upgrade google-api-python-client oauth2client`.
+2. Goto [Gmail's Python quickstart page](https://developers.google.com/gmail/api/quickstart/python), click "ENABLE THE GMAIL API" button. Put the downloaded `credentials.json` file in the same directory of this project.
+3. Run `python del_mail.py`, it will open a webpage. Login to Gmail and click agree.
+4. Input the keyword you want to delete. The script will show some mails that match the keyword. Press enter will delete all the mails that match the keyword.
+
